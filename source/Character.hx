7eaf7dab9213fba303365c861324d88a825c0134
ARAS AKGÜL <94735664+aras123457@users.noreply.github.com>
2022-04-03 09:35:08 +0300

    Add files via upload

--- source/Character.hx
+++ source/Character.hx
@@ -122,15 +122,15 @@ class Character extends FlxSprite
 				updateHitbox();
 				antialiasing = false;
 
-			case 'dad':
+			case 'whitty':
 				// DAD ANIMATION LOADING CODE
-				tex = Paths.getSparrowAtlas('characters/DADDY_DEAREST', 'shared');
+				tex = Paths.getSparrowAtlas('characters/WhittySprites', 'shared');
 				frames = tex;
-				animation.addByPrefix('idle', 'Dad idle dance', 24);
+				animation.addByPrefix('Idle', 'Idle', 24);
 				animation.addByPrefix('singUP', 'Dad Sing Note UP', 24);
-				animation.addByPrefix('singRIGHT', 'Dad Sing Note RIGHT', 24);
-				animation.addByPrefix('singDOWN', 'Dad Sing Note DOWN', 24);
-				animation.addByPrefix('singLEFT', 'Dad Sing Note LEFT', 24);
+				animation.addByPrefix('singRIGHT', 'Sing Right', 24);
+				animation.addByPrefix('singDOWN', 'Sing Down', 24);
+				animation.addByPrefix('singLEFT', 'Sing LeftT', 24);
 
 				addOffset('idle');
 				addOffset("singUP", -6, 50);
